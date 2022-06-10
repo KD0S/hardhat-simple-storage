cdb377fee57698f49a04836fb158cf4981557a45
Krishnadas <Krishnadas.nair02@gmail.com>
2022-06-10 16:09:57 +0400

    First Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,43 @@
+const { ethers, run, network } = require("hardhat");
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying Contract.....");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed Contract to : ${simpleStorage.address}`);
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block Confirmations....");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
+
+  const currentVlaue = await simpleStorage.retrieve();
+  console.log(`Current Value is ${currentVlaue}`);
+
+  const transactionResponse = await simpleStorage.store(89);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Value is ${updatedValue}`);
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying Contract.....");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified"))
+      console.log("Already Verified");
+    else console.log(e);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
